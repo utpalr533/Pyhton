dbacab722b3e962cd4859b8b5d7d494095ea4872
utpalr533 <127889312+utpalr533@users.noreply.github.com>
2025-08-28 08:43:23 +0530

    Add files via upload

---
+++ 01_basics.py
@@ -0,0 +1,3 @@
+age = 34 #integer
+name = "Utpal"
+cgpa= 4.55 #float
---
+++ 02 data types.py
@@ -0,0 +1,12 @@
+age=4 
+print(age)
+print(type(age))
+cgpa = 4.55
+print(cgpa)
+print(type(cgpa))
+name="Utpal"
+print(name)
+print(type(name))
+is_completed = True
+print(is_completed)
+print(type(is_completed))
