c27b19890362f95a49f6bec22ba69413141a7c8f
utpalr533 <127889312+utpalr533@users.noreply.github.com>
2025-08-28 08:45:52 +0530

    Operators in Python

---
+++ operators.py
@@ -0,0 +1,47 @@
+a = 34
+
+b = 2 
+
+# Arithmetic Operators
+# These operators perform mathematical operations on numeric values.
+print("a + b = ", a + b)  # Addition
+print("a - b = ", a - b)  # Subtraction
+print("a * b = ", a * b)  # Multiplication
+print("a / b = ", a / b)  # Division        
+print("a // b = ", a // b)  # Floor Division
+print("a % b = ", a % b)  # Modulus    
+print("a ** b = ", a ** b)  # Exponentiation
+
+# Conditional Operators
+# These operators evaluate conditions and return Boolean values.
+print("a == b = ", a == b)  # Equal to
+print("a != b = ", a != b)  # Not equal to
+print("a > b = ", a > b)    # Greater than
+print("a < b = ", a < b)    # Less than
+print("a >= b = ", a >= b)  # Greater than or equal to
+print("a <= b = ", a <= b)  # Less than or equal to
+print(a!=34)
+
+# Logical Operators
+# These operators combine multiple conditions.
+
+c = True
+d = False   
+print (c and c)
+print (c and d)
+print(d and c)
+print(d and d)
+
+
+print(True and True)
+print(True and False)
+print(False and True)   
+print(False and False)
+
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print("Hello python learners \n I am good")
